47b357cd112e7b8030f7b25127ef54e21ae96c2c
Bugarin Florin <bugarinflorin@gmail.com>
2025-01-28 12:23:31 +0200

    added serializableDictionary

---
+++ Assets/Library/SerializableDictionary/DrawKeyAsPropertyAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace RetroCloud.BioVirus.Templates.SerializableDictionary
+{
+    /// <summary>
+    /// Attribute used to force drawing a key as a property
+    /// </summary>
+    [System.AttributeUsage(System.AttributeTargets.Field)]
+    public class DrawKeyAsPropertyAttribute : System.Attribute { }
+}
